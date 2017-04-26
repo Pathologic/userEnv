56d699fb3b27bd8a058fec351920627bbdc5aa5c
Pathologic <maxx@np.by>
2017-04-26 18:49:28 +0300

    initial

---
+++ assets/snippets/userEnv/snippet.userEnv.php
@@ -0,0 +1,19 @@
+<?php
+$env = array();
+include_once(MODX_BASE_PATH.'assets/lib/APIHelpers.class.php');
+include_once(MODX_BASE_PATH.'assets/snippets/userEnv/SxGeo/SxGeo.php');
+$SxGeo = new SxGeo(MODX_BASE_PATH.'assets/snippets/userEnv/SxGeo/SxGeoCity.dat');
+$env['ip'] = \APIHelpers::getUserIP();
+$env['browser'] = $_SERVER['HTTP_USER_AGENT'];
+if ($geo = $SxGeo->getCityFull($env['ip'])) {
+    $env['coords'] = str_replace(',','.',$geo['city']['lat']).','.str_replace(',','.',$geo['city']['lon']);
+    $env['city'] = $geo['city']['name_ru'];
+    $env['region'] = $geo['region']['name_ru'];
+    $env['country'] = $geo['country']['name_ru'];
+};
+if (isset($FormLister)) {
+    $FormLister->setFields($env,'env');
+} elseif (isset($tpl)) {
+    include_once(MODX_BASE_PATH.'assets/snippets/DocLister/lib/DLTemplate.class.php');
+    return DLTemplate::getInstance($modx)->parseChunk($tpl,$env);
+}
---
+++ install/assets/snippets/userEnv.tpl
@@ -0,0 +1,15 @@
+//<?php
+/**
+ * userEnv
+ * 
+ * snippet to get user environment data
+ *
+ * @category    snippet
+ * @version     1.0.0
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
+ * @internal    @properties 
+ * @internal    @modx_category Content
+ * @internal    @installset base, sample
+ */
+
+return require MODX_BASE_PATH.'assets/snippets/userEnv/snippet.userEnv.php';
